7057e619e3136aaa9f090800c408d5fb606e302a
ember arlynx <ember@lunar.town>
2022-04-04 16:28:36 -0400

    switch over to websocketseses

--- src/lib.rs
+++ src/lib.rs
@@ -134,129 +134,180 @@ impl Game {
         d.concat()
     }
 
-    fn intel_reveal(&self, pid: PlayerId, update_queue: &mut Vec<(Option<PlayerId>, ClientUpdate)>, intel_kind: IntelKind) {
-      let kind = if self.players[pid].hidden_signals {
-        None
-      } else {
-        Some(intel_kind)
-      };
-      for pl in &self.players {
-        if pl.id != pid {
-          update_queue.push((Some(pl.id), ClientUpdate::Intel { by: Some(pid), kind } ));
+    fn intel_reveal(
+        &self,
+        pid: PlayerId,
+        update_queue: &mut Vec<(Option<PlayerId>, ClientUpdate)>,
+        intel_kind: IntelKind,
+    ) {
+        let kind = if self.players[pid].hidden_signals {
+            None
+        } else {
+            Some(intel_kind)
+        };
+        for pl in &self.players {
+            if pl.id != pid {
+                update_queue.push((
+                    Some(pl.id),
+                    ClientUpdate::Intel {
+                        by: Some(pid),
+                        kind,
+                    },
+                ));
+            }
         }
-      }
     }
 
     pub fn strike(&mut self, pid: PlayerId) -> Vec<(Option<PlayerId>, ClientUpdate)> {
-      let mut update_queue = Vec::new();
-      for pl in 0..self.players.len() {
-        if pl != pid {
-          if self.players[pid].location == self.players[pl].location {
-              self.players[pl].alive = false;
-              update_queue.push((Some(pl), ClientUpdate::Death { by: pid, of: pl }));
-              update_queue.push((Some(pid), ClientUpdate::Death { by: pid, of: pl }));
-          }
-          if self.players[pl].visible_violence || !self.players[pl].alive {
-            update_queue.push((Some(pl), ClientUpdate::Strike { by: Some(pid), at: Some(self.players[pid].location) }));
-          } else {
-            update_queue.push((Some(pl), ClientUpdate::Strike { by: Some(pid), at: None }));
-          }
+        let mut update_queue = Vec::new();
+        for pl in 0..self.players.len() {
+            if pl != pid {
+                if self.players[pid].location == self.players[pl].location {
+                    self.players[pl].alive = false;
+                    update_queue.push((Some(pl), ClientUpdate::Death { by: pid, of: pl }));
+                    update_queue.push((Some(pid), ClientUpdate::Death { by: pid, of: pl }));
+                }
+                if self.players[pl].visible_violence || !self.players[pl].alive {
+                    update_queue.push((
+                        Some(pl),
+                        ClientUpdate::Strike {
+                            by: Some(pid),
+                            at: Some(self.players[pid].location),
+                        },
+                    ));
+                } else {
+                    update_queue.push((
+                        Some(pl),
+                        ClientUpdate::Strike {
+                            by: Some(pid),
+                            at: None,
+                        },
+                    ));
+                }
+            }
         }
-      }
-      update_queue
+        update_queue
     }
 
     pub fn wait(&mut self, pid: PlayerId) -> Vec<(Option<PlayerId>, ClientUpdate)> {
-      vec![(None, ClientUpdate::WaitMove { by: Some(pid) })]
+        vec![(None, ClientUpdate::WaitMove { by: Some(pid) })]
     }
 
     pub fn capture(&mut self, pid: PlayerId) -> Vec<(Option<PlayerId>, ClientUpdate)> {
-      self.cities.node_weight_mut(self.players[pid].location).unwrap().control = Some(pid);
-      vec![(None, ClientUpdate::Capture { by: pid, at: self.players[pid].location })]
+        self.cities
+            .node_weight_mut(self.players[pid].location)
+            .unwrap()
+            .control = Some(pid);
+        vec![(
+            None,
+            ClientUpdate::Capture {
+                by: pid,
+                at: self.players[pid].location,
+            },
+        )]
     }
 
     pub fn hide_signals(&mut self, pid: PlayerId) -> Vec<(Option<PlayerId>, ClientUpdate)> {
-      // TODO: spend intel
-      // TODO: can't do this twice
-      let mut update_queue = Vec::new();
-      self.intel_reveal(pid, &mut update_queue, IntelKind::HideSignals);
-      self.players[pid].hidden_signals = true;
-      update_queue
+        // TODO: spend intel
+        // TODO: can't do this twice
+        let mut update_queue = Vec::new();
+        self.intel_reveal(pid, &mut update_queue, IntelKind::HideSignals);
+        self.players[pid].hidden_signals = true;
+        update_queue
     }
 
-    pub fn reveal(&mut self, pid: PlayerId, reveal: Option<PlayerId>) -> Vec<(Option<PlayerId>, ClientUpdate)> {
-      // TODO: spend intel
-      let mut update_queue = Vec::new();
-      if let Some(reveal) = reveal {
-        if !self.players[reveal].invisible {
-          update_queue.push((Some(pid), ClientUpdate::Reveal { who: reveal, at: self.players[reveal].location }));
-        } else {
-          update_queue.push((Some(pid), ClientUpdate::RevealFailure { who: reveal } ));
-        }
-      } else {
-        for reveal in &self.players {
-          if reveal.id != pid {
-            if !reveal.invisible {
-              update_queue.push((Some(pid), ClientUpdate::Reveal { who: reveal.id, at: reveal.location }));
+    pub fn reveal(
+        &mut self,
+        pid: PlayerId,
+        reveal: Option<PlayerId>,
+    ) -> Vec<(Option<PlayerId>, ClientUpdate)> {
+        // TODO: spend intel
+        let mut update_queue = Vec::new();
+        if let Some(reveal) = reveal {
+            if !self.players[reveal].invisible {
+                update_queue.push((
+                    Some(pid),
+                    ClientUpdate::Reveal {
+                        who: reveal,
+                        at: self.players[reveal].location,
+                    },
+                ));
             } else {
-              update_queue.push((Some(pid), ClientUpdate::RevealFailure { who: reveal.id } ));
+                update_queue.push((Some(pid), ClientUpdate::RevealFailure { who: reveal }));
             }
-          }
+        } else {
+            for reveal in &self.players {
+                if reveal.id != pid {
+                    if !reveal.invisible {
+                        update_queue.push((
+                            Some(pid),
+                            ClientUpdate::Reveal {
+                                who: reveal.id,
+                                at: reveal.location,
+                            },
+                        ));
+                    } else {
+                        update_queue
+                            .push((Some(pid), ClientUpdate::RevealFailure { who: reveal.id }));
+                    }
+                }
+            }
         }
-      }
-      self.intel_reveal(pid, &mut update_queue, IntelKind::HideSignals);
-      update_queue
+        self.intel_reveal(pid, &mut update_queue, IntelKind::HideSignals);
+        update_queue
     }
 
     pub fn invisible(&mut self, pid: PlayerId) -> Vec<(Option<PlayerId>, ClientUpdate)> {
-      // TODO: spend intel
-      let mut update_queue = Vec::new();
-      self.players[pid].invisible = true;
-      self.intel_reveal(pid, &mut update_queue, IntelKind::Invisible);
-      update_queue
+        // TODO: spend intel
+        let mut update_queue = Vec::new();
+        self.players[pid].invisible = true;
+        self.intel_reveal(pid, &mut update_queue, IntelKind::Invisible);
+        update_queue
     }
 
     pub fn prepare(&mut self, pid: PlayerId) -> Vec<(Option<PlayerId>, ClientUpdate)> {
-      // TODO: spend intel
-      let mut update_queue = Vec::new();
-      // TODO
-      self.intel_reveal(pid, &mut update_queue, IntelKind::Prepare);
-      update_queue
+        // TODO: spend intel
+        let mut update_queue = Vec::new();
+        // TODO
+        self.intel_reveal(pid, &mut update_queue, IntelKind::Prepare);
+        update_queue
     }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum ClientUpdate {
-  Death {
-    by: PlayerId,
-    of: PlayerId,
-  },
-  Strike {
-    by: Option<PlayerId>,
-    at: Option<NodeIndex>,
-  },
-  WaitMove { by: Option<PlayerId> },
-  Capture {
-    by: PlayerId,
-    at: NodeIndex,
-  },
-  Intel {
-    by: Option<PlayerId>,
-    kind: Option<IntelKind>,
-  },
-  Reveal {
-    who: PlayerId,
-    at: NodeIndex,
-  },
-  RevealFailure {
-    who: PlayerId,
-  },
+    Death {
+        by: PlayerId,
+        of: PlayerId,
+    },
+    Strike {
+        by: Option<PlayerId>,
+        at: Option<NodeIndex>,
+    },
+    WaitMove {
+        by: Option<PlayerId>,
+    },
+    Capture {
+        by: PlayerId,
+        at: NodeIndex,
+    },
+    Intel {
+        by: Option<PlayerId>,
+        kind: Option<IntelKind>,
+    },
+    Reveal {
+        who: PlayerId,
+        at: NodeIndex,
+    },
+    RevealFailure {
+        who: PlayerId,
+    },
 }
 
 #[derive(Debug, Copy, Clone, Serialize, Deserialize)]
 pub enum IntelKind {
-  HideSignals,
-  Reveal,
-  Invisible,
-  Prepare,
+    HideSignals,
+    Reveal,
+    Invisible,
+    Prepare,
 }
--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,4 @@
+use actix::prelude::*;
 use actix_session::{storage::CookieSessionStore, SessionMiddleware};
 use actix_web::cookie::Key;
 use actix_web::web::{Bytes, Data};
@@ -5,29 +6,31 @@ use actix_web::{
     get, http::header::ContentType, middleware::Logger, web, App, HttpResponse, HttpServer,
     Responder,
 };
-use actix_web::{post, http::header};
+use actix_web::{http::header, post};
+use actix_web::{Error, HttpRequest};
+use actix_web_actors::ws;
 use parking_lot::Mutex;
 use petgraph::graph::NodeIndex;
 use std::collections::BTreeMap;
 use std::process::Stdio;
 use std::sync::Arc;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::sync::mpsc::{Sender, Receiver};
+use tokio::sync::mpsc::{Receiver, Sender};
 
 use tokio::process::Command;
 
 struct GameState {
-  game: Arc<Mutex<esgea::Game>>,
-  pid_channels: Vec<Option<Sender<Bytes>>>,
+    game: Arc<Mutex<esgea::Game>>,
+    pid_channels: Vec<Option<Addr<ReceiverStream>>>,
 }
 
 impl GameState {
-  fn new() -> Self {
-    Self {
-      game: Arc::new(Mutex::new(esgea::Game::new())),
-      pid_channels: vec![],
+    fn new() -> Self {
+        Self {
+            game: Arc::new(Mutex::new(esgea::Game::new())),
+            pid_channels: vec![],
+        }
     }
-  }
 }
 
 struct State {
@@ -47,8 +50,7 @@ async fn index() -> impl Responder {
 async fn start_game(state: Data<Mutex<State>>) -> impl Responder {
     let mut st = state.lock();
     let gid: u128 = rand::random();
-    st.games
-        .insert(gid, GameState::new());
+    st.games.insert(gid, GameState::new());
     HttpResponse::Ok()
         .append_header(ContentType::plaintext())
         .body(format!("{}", gid))
@@ -66,42 +68,62 @@ async fn list_games(state: Data<Mutex<State>>) -> impl Responder {
     )
 }
 
-struct ReceiverStream(Receiver<Bytes>);
+struct ReceiverStream;
 
 impl core::ops::Drop for ReceiverStream {
-  fn drop(&mut self) {
-    println!("dropping a channel");
-  }
+    fn drop(&mut self) {
+        println!("dropping a channel");
+    }
 }
 
-impl futures_util::Stream for ReceiverStream {
-  type Item = Result<Bytes, actix_web::Error>;
+impl Actor for ReceiverStream {
+    type Context = ws::WebsocketContext<Self>;
+}
 
-  fn poll_next(mut self: core::pin::Pin<&mut Self>, cx: &mut core::task::Context<'_>) -> core::task::Poll<Option<Self::Item>> {
-    use core::task::Poll::*;
-    match core::pin::Pin::new(&mut self.0).poll_recv(cx) {
-      Ready(Some(b)) => { 
-        println!("dispatching some bytes: {b:?}");
-        Ready(Some(Ok(b)))
-      },
-      Ready(None) => {
-        println!("closing channel");
-        Ready(None)
-      },
-      Pending => Pending,
+/// Handler for `ws::Message`
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for ReceiverStream {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        match msg {
+            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
+            _ => {}
+        }
     }
-  }
+}
+
+struct Upd(usize, esgea::ClientUpdate);
+impl Message for Upd {
+    type Result = ();
+}
+
+impl Handler<Upd> for ReceiverStream {
+    type Result = ();
+    fn handle(&mut self, msg: Upd, ctx: &mut Self::Context) {
+        ctx.text(serde_json::to_string(&(msg.0, msg.1)).expect("jsonify reactor supercritical"))
+    }
 }
 
 #[get("/events/{gid}/{pid}")]
-async fn event_stream(state: Data<Mutex<State>>, path: web::Path<(String, String)>) -> impl Responder {
-  let (tx, rx) = tokio::sync::mpsc::channel(100);
-  let (gid, pid) = path.into_inner();
-  let gid: u128 = gid.parse().expect("sad gid");
-  let pid: esgea::PlayerId = pid.parse().expect("sad pid");
-  println!("getting event stream for {gid}/{pid}");
-  state.lock().games.entry(gid).and_modify(|e| if pid < e.pid_channels.len() { e.pid_channels[pid] = Some(tx) });
-  HttpResponse::Ok().append_header((header::CONTENT_TYPE, "text/event-stream")).streaming(ReceiverStream(rx))
+async fn event_stream(
+    state: Data<Mutex<State>>,
+    req: HttpRequest,
+    path: web::Path<(String, String)>,
+    stream: web::Payload,
+) -> Result<HttpResponse, Error> {
+    let (gid, pid) = path.into_inner();
+    let gid: u128 = gid.parse().expect("sad gid");
+    let pid: esgea::PlayerId = pid.parse().expect("sad pid");
+    println!("getting event stream for {gid}/{pid}");
+    let actor = ReceiverStream;
+    let mut res = ws::handshake(&req)?;
+
+    let (addr, stream) = ws::WebsocketContext::create_with_addr(actor, stream);
+    state.lock().games.entry(gid).and_modify(|e| {
+        if pid < e.pid_channels.len() {
+            e.pid_channels[pid] = Some(addr)
+        }
+    });
+
+    Ok(res.streaming(stream))
 }
 
 #[post("/join_game/{gid}")]
@@ -114,10 +136,15 @@ async fn join_game(state: Data<Mutex<State>>, path: web::Path<String>) -> impl R
         Some(gm) => {
             gm.pid_channels.push(None);
             let mut gm = gm.game.lock();
-            let new_player = gm.players.last().cloned().map(|last| esgea::Player {
-                id: last.id + 1,
-                ..last
-            }).unwrap_or(Default::default());
+            let new_player = gm
+                .players
+                .last()
+                .cloned()
+                .map(|last| esgea::Player {
+                    id: last.id + 1,
+                    ..last
+                })
+                .unwrap_or(Default::default());
             println!("adding player to game {gid}: {new_player:?}");
             gm.players.push(new_player);
             gm.updates.push(vec![]);
@@ -136,7 +163,13 @@ async fn render(state: Data<Mutex<State>>, path: web::Path<(String, String)>) ->
     let gid: u128 = gid.parse().expect("gid isnt u128");
     let pid: esgea::PlayerId = pid.parse().expect("pid isnt usize");
 
-    let graphviz_source = st.games.get(&gid).expect("no game?").game.lock().render(pid);
+    let graphviz_source = st
+        .games
+        .get(&gid)
+        .expect("no game?")
+        .game
+        .lock()
+        .render(pid);
     let mut child = Command::new("dot")
         .arg("-Tsvg")
         .stdout(Stdio::piped())
@@ -157,35 +190,39 @@ async fn render(state: Data<Mutex<State>>, path: web::Path<(String, String)>) ->
         .body(svg)
 }
 
-async fn distribute_updates(gs: &mut GameState, updates: Vec<(Option<esgea::PlayerId>, esgea::ClientUpdate)>) {
-  let mut game = gs.game.lock();
-  for (pid, upd) in updates {
-    if let Some(pid) = pid {
-      let seqno = game.updates[pid].len();
-      game.updates[pid].push(upd.clone());
-      if let Some(tx) = &gs.pid_channels[pid] {
-        let result = tx.send(Bytes::copy_from_slice(serde_json::to_string(&(seqno, upd)).expect("cannot json encode client update").as_bytes())).await;
-        if let Err(_) = result {
-          gs.pid_channels[pid] = None;
-        }
-      } else {
-        println!("no active event stream for {pid} -- cannot send {upd:?}");
-      }
-    } else {
-      for pl in 0..game.updates.len() {
-        let seqno = game.updates[pl].len();
-        game.updates[pl].push(upd.clone());
-        if let Some(tx) = &gs.pid_channels[pl] {
-          let result = tx.send(Bytes::copy_from_slice(serde_json::to_string(&(seqno, upd.clone())).expect("cannot json encode client update").as_bytes())).await;
-          if let Err(_) = result {
-            gs.pid_channels[pl] = None;
-          }
+async fn distribute_updates(
+    gs: &mut GameState,
+    updates: Vec<(Option<esgea::PlayerId>, esgea::ClientUpdate)>,
+) {
+    let mut game = gs.game.lock();
+    for (pid, upd) in updates {
+        if let Some(pid) = pid {
+            let seqno = game.updates[pid].len();
+            game.updates[pid].push(upd.clone());
+            if let Some(tx) = &gs.pid_channels[pid] {
+                let result = tx.send(Upd(seqno, upd)).await;
+                if let Err(eeeeee) = result {
+                    println!("{} sending to {}, dropping delivery", eeeeee, pid);
+                    gs.pid_channels[pid] = None;
+                }
+            } else {
+                println!("no active event stream for {pid} -- cannot send {upd:?}");
+            }
         } else {
-          println!("no active event stream for {pl} -- cannot send {upd:?}");
+            for pl in 0..game.updates.len() {
+                let seqno = game.updates[pl].len();
+                game.updates[pl].push(upd.clone());
+                if let Some(tx) = &gs.pid_channels[pl] {
+                    let result = tx.send(Upd(seqno, upd.clone())).await;
+                    if let Err(_) = result {
+                        gs.pid_channels[pl] = None;
+                    }
+                } else {
+                    println!("no active event stream for {pl} -- cannot send {upd:?}");
+                }
+            }
         }
-      }
     }
-  }
 }
 
 #[post("/do_action/{gid}/{pid}")]
@@ -201,32 +238,51 @@ async fn do_action(
     let mut guard = state.lock();
     let mut gs = guard.games.get_mut(&gid).expect("no homie");
     match body.as_ref() {
-        b"strike" => { let upds = gs.game.lock().strike(pid); distribute_updates(&mut gs, upds).await },
-        b"wait" => { let upds = gs.game.lock().wait(pid); distribute_updates(&mut gs, upds).await },
-        b"capture" => { let upds = gs.game.lock().capture(pid); distribute_updates(&mut gs, upds).await },
-        b"hide_signals" => { let upds = gs.game.lock().hide_signals(pid); distribute_updates(&mut gs, upds).await },
-        b"invisible" => { let upds = gs.game.lock().invisible(pid); distribute_updates(&mut gs, upds).await },
-        b"prepare" => { let upds = gs.game.lock().prepare(pid); distribute_updates(&mut gs, upds).await },
+        b"strike" => {
+            let upds = gs.game.lock().strike(pid);
+            distribute_updates(&mut gs, upds).await
+        }
+        b"wait" => {
+            let upds = gs.game.lock().wait(pid);
+            distribute_updates(&mut gs, upds).await
+        }
+        b"capture" => {
+            let upds = gs.game.lock().capture(pid);
+            distribute_updates(&mut gs, upds).await
+        }
+        b"hide_signals" => {
+            let upds = gs.game.lock().hide_signals(pid);
+            distribute_updates(&mut gs, upds).await
+        }
+        b"invisible" => {
+            let upds = gs.game.lock().invisible(pid);
+            distribute_updates(&mut gs, upds).await
+        }
+        b"prepare" => {
+            let upds = gs.game.lock().prepare(pid);
+            distribute_updates(&mut gs, upds).await
+        }
         _ => match body.as_ref().split(|c| b':' == *c).collect::<Vec<_>>()[..] {
             [b"move", to] => {
                 return HttpResponse::Ok().body(
                     // TODO: fix try_move to give events
-                    gs.game.lock().try_move(
-                        pid,
-                        NodeIndex::new(
-                            std::str::from_utf8(to)
-                                .expect("utf8")
-                                .parse()
-                                .expect("bad location"),
-                        ),
-                    )
-                    .to_string(),
+                    gs.game
+                        .lock()
+                        .try_move(
+                            pid,
+                            NodeIndex::new(
+                                std::str::from_utf8(to)
+                                    .expect("utf8")
+                                    .parse()
+                                    .expect("bad location"),
+                            ),
+                        )
+                        .to_string(),
                 );
             }
             [b"reveal", who] => {
                 gs.game.lock().reveal(
-                    pid,
-                    // TODO
+                    pid, // TODO
                     None,
                 );
             }
